824bd16907661d8bd0d0b5c71b0b4a51346be407
Arda Ozan Yildiz <6242559@collegemv.qc.ca>
2025-02-18 13:10:23 -0500

    avancer page accueil

--- frontend/src/pages/HomePages.jsx
+++ frontend/src/pages/HomePages.jsx
@@ -3,25 +3,33 @@ import './HomePages.css';
 
 function HomePages(props) {
     return (
-        
         <div>
-            {/* <img src="./src/image/background.jpg" style={{backgroundSize:"cover"}}/> */}
+            <div class="message-container">
+                <h1>Aventure toi dans le monde de la musique et profite en pour jouer a un petit jeu</h1>
+            </div>
+            <div class="card-container">
+        
+        
+                <div class="card">
+                    <p class="heading">Tuto Recherche</p>
+                    <p>Si vous avez besoin de voir un court tutoriel sur comment rechercher des informations sur des artistes.</p>
+                    <p>Cliquez ici</p>
+                </div>
             
-        <div class="card">
-        <p class="heading">
-            Popular this month
-        </p>
-        <p>
-            Powered By
-        </p>
-        <p>Uiverse
-        </p></div>
-
-    
-        </div>
-
-
+                <div class="card">
+                    <p class="heading">Tuto Jeux</p>
+                    <p>Si vous n'avez encore jamais jouer à notre petit jeux, il est très conseillé de regarder le tutoriel avant de commencer.</p>
+                    <p>Cliquez ici</p>
+                </div>
+            </div>
+      </div>
+   
     );
 }
 
 export default HomePages;
+
+
+
+
+
